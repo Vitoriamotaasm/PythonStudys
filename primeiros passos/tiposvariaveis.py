9dc9bfc9e2120a1c39b9c3a395a101bd789406e9
Vitoriamotaasm <luanavitoriadasilvamota@gmail.com>
2025-07-08 16:03:53 -0300

    Tipos de variaveis

---
+++ primeiros passos/tiposvariaveis.py
@@ -0,0 +1,16 @@
+"""
+str - string
+int - inteiro
+float - reais
+bool - verdadeiro ou falso
+list - lista de nomes, numeros, etc. 
+"""
+print(type("Ester"))
+print(type(6))
+print(type(14.67))
+print(type(True))
+
+letras = ["A", "B", "C"]
+print(type(letras))
+
+print("Bia", type(Bia))
