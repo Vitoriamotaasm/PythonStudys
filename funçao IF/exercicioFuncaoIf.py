012dc900ea0ead67ea1d194633c2fa95cf88726f
Vitoriamotaasm <luanavitoriadasilvamota@gmail.com>
2025-07-10 18:45:43 -0300

    Exercicio de condicionais

---
+++ funçao IF/exercicioFuncaoIf.py
@@ -0,0 +1,12 @@
+pontuacao = 90
+
+if pontuacao >= 90:
+    print("Sua classificação é A. Parabéns!")
+elif pontuacao >= 80 < 90:
+    print("Sua classificação é B. Muito bom!")
+elif pontuacao >= 70 < 80:
+    print("Sua classificação é C. Bom trabalho!")
+elif pontuacao >= 60 < 70:
+    print("Sua classificação é D. Precisa melhorar!")
+elif pontuacao < 60:
+    print("Sua classificação é F. Estude mais!")
