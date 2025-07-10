e7e83816b3224e991672248eac8313f5fc2e9839
Vitoriamotaasm <luanavitoriadasilvamota@gmail.com>
2025-07-10 11:12:22 -0300

    Operadores de atribuicao

---
+++ primeiros passos/operadoresAtribuicao.py
@@ -0,0 +1,32 @@
+""""
+= numero = 1 / Recebe
++= numero = numero + 1 / Incremento
+-= numero = numero - 1 / Decremento
+*= numero = numero * 1 / Multiplicando
+/= numero = numero / 1 / Divide
+%= numero = numero % 1 / Resto
+"""
+
+#Operador =
+numero = 10
+print(numero)
+
+#Operador +=
+numero += 1
+print(numero)
+
+#Operador -=
+numero -= 1
+print(numero)
+
+#Operador *=
+numero *= 2
+print(numero)
+
+#Operador /=
+numero /= 2
+print(numero)
+
+#Operador %=
+numero %= 7
+print(numero)
