744a9884798361e55d9dac1bdf28c41721637ccf
Vitoriamotaasm <luanavitoriadasilvamota@gmail.com>
2025-07-10 17:11:01 -0300

    Operadores relacionais

---
+++ funçao IF/operadoresRelacionais.py
@@ -0,0 +1,39 @@
+""""
+== Igual: 
+!+ Diferente de: 
+> Maior que: 
+>= Maior ou igual: 
+< Menor que: 
+<= Menor ou igual: 
+"""
+
+#Operador ==
+letra = "a"
+if letra == "a": #if = Se
+    print("A letra é igual a letra A")
+
+#Operador !=
+if letra != "a": #if = Se
+    print("A letra é diferente de A")
+
+#Operador >
+numero1 = 20
+numero2 = 15
+if numero1 > numero2:
+    print("O numero 1 é maior que o numero 2")
+
+#Operador <
+n1 = 10
+n2 = 30
+if n1 < n2:
+    print("O numero 2 é maior que o numero 1")
+
+#Operador >=
+num1 = 50
+num2 = 40
+if num1 >= 20:
+    print("O numero 1 é maior ou igual a 20")
+
+#Operador <= 
+if num2 <= 15:
+    print("O numero 2 é menor ou igual a 15")
