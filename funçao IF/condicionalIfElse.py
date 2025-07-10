7ab42a927e0c76ca6de4e59135e100ebbcb245bd
Vitoriamotaasm <luanavitoriadasilvamota@gmail.com>
2025-07-10 18:32:53 -0300

    Condicoes If Else Elif

---
+++ funçao IF/condicionalIfElse.py
@@ -0,0 +1,22 @@
+n1 = 5
+n2 = 50
+
+if n1 > n2:
+    print("n1 é maior que n2")
+elif n1 == n2:
+    print("n1 e n2 sao iguais")
+
+numero1 = 10
+numero2 = 20
+
+if numero1 > numero2:
+    print("numero1 é maior que numero2")
+elif numero1 == numero2:
+    print("numero1 e numero2 sao iguais")
+else:
+    print("Nenhuma das condicoes atende ao criterio")
+
+num1 = 5
+num2 = 12
+
+print("num1 é maior") if num1 > num2 else print("num2 é maior")
