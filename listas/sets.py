6832eaad7124448aaed297f6c4e5f6a589f9f04a
Vitoriamotaasm <luanavitoriadasilvamota@gmail.com>
2025-07-11 11:03:08 -0300

    Sets 2

--- listas/sets.py
+++ listas/sets.py
@@ -25,3 +25,44 @@ print(imprimindo)
 
 valorEmAmbos = listaSet1.intersection(listaSet2)
 print(valorEmAmbos)
+
+#symmetric_difference_update: itens diferentes
+listaSet1.symmetric_difference_update(listaSet2)
+print(listaSet1)
+
+#symmetri_difference: itens diferentes em ambos conjuntos
+listaS1 = {"Python", "C++", "Java"}
+listaS2 = {"VisualG", "Logica", "Python"}
+naoEstaoEmAmbas = listaS1.symmetric_difference(listaS2)
+print(naoEstaoEmAmbas)
+
+#Sets nao aceitam valores repetidos
+setNumero = {1, 2, 3, 4, 5, 6, 6, 7, 8}
+print(setNumero)
+print(len(setNumero))
+
+setExemplo1 = {"A", "B", "C"}
+setExemplo2 = {1, 2, 3}
+setExemplo3 = {True, False, True}
+setExemplo4 = {"Maca", 12, True}
+setExemplo2.update(setExemplo1) #unindo os sets
+print(setExemplo1)
+print(setExemplo2)
+print(setExemplo3)
+print(setExemplo4)
+
+#imprimindo sets com for
+listaObjetos = {"Casa", "Moto", "Bicicleta", "Lancha"}
+for i in listaObjetos:
+    print(i)
+#add item no set
+listaObjetos.add("Carro")
+print(listaObjetos)
+#removendo item no set
+listaObjetos.remove("Moto")
+print(listaObjetos)
+#removendo o ultimo item do set
+listaObjetos.pop()
+print(listaObjetos)
+#verificar se existe um item no set
+print("Bicicleta" in listaObjetos)
