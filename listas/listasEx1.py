75081ef5f47d39276fc97367c42867e7700b2f54
Vitoriamotaasm <luanavitoriadasilvamota@gmail.com>
2025-07-11 09:03:27 -0300

    Listas, exemplo 1

---
+++ listas/listasEx1.py
@@ -0,0 +1,26 @@
+listaNome = ["Amanda", "Clarisse", "Roger", "Allan"]
+print(listaNome)
+print(len(listaNome)) #len apresenta quantos elementos existem na lista
+
+lista1 = ["A", "B", "C"]
+lista2 = [1, 2, 3 ,4]
+lista3 = [True, False, False]
+lista4 = ["Roberta", 34, False]
+print(lista1)
+print(lista2)
+print(lista3)
+print(lista4)
+
+listaTeste = ["A", "Banana", "C", "D", "Elefante"]
+print(listaTeste[0]) 
+print(listaTeste[1])
+print(listaTeste[2])
+print(listaTeste[3])
+print(listaTeste[4])    
+
+print(listaTeste[-1]) #ultimo intem da lista
+print(listaTeste[1:3]) #do indice 1 ao 2 (3 n)
+print(listaTeste[:3]) #os 3 primeiros
+print(listaTeste[2:]) #a partir do 3 item
+print(listaTeste[::2]) #nao declarei nem o inicio e nem o fim, apenas o step. Ou seja, de dois em dois
+print(listaTeste[::-1]) #lista invertida
